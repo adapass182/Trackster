6a058071c7526af1914094f9c8742b7ce5cecb73
adapass182 <adampassingham90@gmail.com>
2018-08-10 17:50:25 +0200

    removed circularprogress loading component in favor of prompt and button to suggest user plays a song in spotify to see nowPlaying

--- client/src/components/NowPlaying.jsx
+++ client/src/components/NowPlaying.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 import { getNowPlaying } from '../actions/spotify_a'
 
 import {
-	CircularProgress,
+	Button,
 	GridListTile,
 	GridListTileBar,
 	ListSubheader,
@@ -23,11 +23,9 @@ const styles = () => ({
 })
 
 class NowPlaying extends PureComponent {
-	componentDidMount() {
-		this.props.getNowPlaying()
-	}
 
 	isPlaying(nowPlaying, classes) {
+		this.props.getNowPlaying()
 		if (this.props.nowPlaying.albumName !== null) {
 			return (
 				<GridListTile
@@ -52,8 +50,18 @@ class NowPlaying extends PureComponent {
 		} else {
 			return (
 				<div>
-					<CircularProgress className={classes.progress} size={50} />
-					<Typography>Loading...</Typography>
+					<Typography>
+						I can't hear anything! Make sure you're playing a song on your
+						Spotify account, then click below and I'll have another listen...
+					</Typography>
+					<Button
+						variant="raised"
+						color="primary"
+						type="submit"
+						onClick={() => this.isPlaying(nowPlaying, classes)}
+					>
+						Listen again
+					</Button>
 				</div>
 			)
 		}
--- client/src/reducers/selectArtist_r.js
+++ client/src/reducers/selectArtist_r.js
@@ -1,7 +1,6 @@
 import { ADD_ARTIST, REM_ARTIST } from '../actions/selector_a'
 
 export default (state = [], { type, payload }) => {
-	console.log('Hi Adam: ' + payload)
 	switch (type) {
 	case ADD_ARTIST:
 		state.push(payload)
