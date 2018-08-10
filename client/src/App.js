13b24372cca01e95ba6b705c228237fe26bfb96b
adapass182 <adampassingham90@gmail.com>
2018-08-10 19:37:40 +0200

    minor refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,29 @@
-import React, { Component } from "react"
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
-import PropTypes from "prop-types"
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-import { withStyles } from "@material-ui/core"
+import { withStyles } from '@material-ui/core'
 
-import "./App.css"
+import './App.css'
 
-import Homepage from "./components/Homepage"
-import Splash from "./components/Splash"
+import Homepage from './components/Homepage'
+import Splash from './components/Splash'
 
 const styles = () => ({
 	root: {
-		backgroundColor: "white"
+		backgroundColor: 'white'
 	}
 })
 
 class App extends Component {
 	render() {
-
 		const { classes } = this.props
 
 		return (
 			<Router>
 				<div className={classes.root}>
 					<Route exact path="/" render={() => <Redirect to="/splash" />} />
-					<Route exact path="/splash" component={Splash} /> 
+					<Route exact path="/splash" component={Splash} />
 					<Route exact path="/homepage" component={Homepage} />
 				</div>
 			</Router>
@@ -36,4 +35,4 @@ App.propTypes = {
 	classes: PropTypes.object.isRequired
 }
 
-export default (withStyles(styles)(App))
+export default withStyles(styles)(App)
