4444f61f2e94585e1afe9101f7a1b8bd1a335539
adapass182 <adampassingham90@gmail.com>
2018-08-11 18:28:18 +0200

    implemented react swipeable view component to render and display track recommendations

--- client/src/components/Recommendations.jsx
+++ client/src/components/Recommendations.jsx
@@ -1,62 +1,132 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { Button, MobileStepper, withStyles } from '@material-ui/core'
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
+import SwipeableViews from 'react-swipeable-views'
 
-import {
-	GridList,
-	GridListTile,
-	GridListTileBar,
-	withStyles,
-	Typography
-} from '@material-ui/core'
-
-const styles = () => ({
-	gridList: {
-		flexWrap: 'nowrap'
+const styles = theme => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		overflow: 'hidden',
+		marginBottom: '2rem'
 	},
-	titleWrap: {
-		overflow: 'visible'
+	header: {
+		display: 'flex',
+		alignItems: 'center',
+		height: 50,
+		paddingLeft: theme.spacing.unit * 4,
+		marginBottom: 20,
+		backgroundColor: theme.palette.background.default
+	},
+	img: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		height: 400,
+		maxWidth: 400,
+		overflow: 'hidden',
+		width: '100%',
+		margin: '3rem auto 0.2rem auto'
 	}
 })
 
-class Recommendations extends PureComponent {
+class Recommendations extends Component {
+	state = {
+		activeStep: 0
+	}
+
 	componentDidUpdate() {
 		window.scrollTo(0, document.body.scrollHeight)
 	}
 
-	isReady() {
-		const { classes, recommendations } = this.props
+	handleNext = () => {
+		this.setState(prevState => ({
+			activeStep: prevState.activeStep + 1
+		}))
+	}
+
+	handleBack = () => {
+		this.setState(prevState => ({
+			activeStep: prevState.activeStep - 1
+		}))
+	}
+
+	handleStepChange = activeStep => {
+		this.setState({ activeStep })
+	}
+
+	render() {
+		const { classes, recommendations, theme } = this.props
+		const { activeStep } = this.state
+
 		if (recommendations === null) {
 			return null
 		} else {
 			return (
-				<div>
-					<Typography>You might like these...</Typography>
-					<GridList className={classes.gridList}>
+				<div className={classes.root}>
+					<SwipeableViews
+						axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+						index={this.state.activeStep}
+						onChangeIndex={this.handleStepChange}
+						enableMouseEvents
+					>
 						{recommendations.tracks.map(track => (
-							<GridListTile key={track.name} cols={2.5}>
-								<img src={track.album.images[0].url} alt={track.name} />
-								<GridListTileBar
-									className={classes.titleWrap}
-									title={track.name}
-									subtitle={track.artists[0].name}
-								/>
-							</GridListTile>
+							<img
+								key={track.id}
+								className={classes.img}
+								src={track.album.images[1].url}
+								alt={track.name}
+							/>
 						))}
-					</GridList>
+					</SwipeableViews>
+					<MobileStepper
+						steps={recommendations.tracks.length}
+						position="static"
+						activeStep={activeStep}
+						className={classes.mobileStepper}
+						nextButton={
+							<Button
+								size="small"
+								onClick={this.handleNext}
+								disabled={activeStep === recommendations.tracks.length - 1}
+							>
+								Next
+								{theme.direction === 'rtl' ? (
+									<KeyboardArrowLeft />
+								) : (
+									<KeyboardArrowRight />
+								)}
+							</Button>
+						}
+						backButton={
+							<Button
+								size="small"
+								onClick={this.handleBack}
+								disabled={activeStep === 0}
+							>
+								{theme.direction === 'rtl' ? (
+									<KeyboardArrowRight />
+								) : (
+									<KeyboardArrowLeft />
+								)}
+								Back
+							</Button>
+						}
+					/>
 				</div>
 			)
 		}
-	}
-
-	render() {
-		return <div>{this.isReady()}</div>
 	}
 }
 
 Recommendations.propTypes = {
 	classes: PropTypes.object.isRequired,
-	recommendations: PropTypes.object.isRequired
+	recommendations: PropTypes.object.isRequired,
+	theme: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => {
@@ -68,4 +138,4 @@ const mapStateToProps = state => {
 export default connect(
 	mapStateToProps,
 	null
-)(withStyles(styles)(Recommendations))
+)(withStyles(styles, { withTheme: true })(Recommendations))
