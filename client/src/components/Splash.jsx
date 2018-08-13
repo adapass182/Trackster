ae9b3225e548a3451a2a11971b7060123bf5b11d
adapass182 <adampassingham90@gmail.com>
2018-08-13 17:49:28 +0200

    added styling to splash page

--- client/src/components/Splash.jsx
+++ client/src/components/Splash.jsx
@@ -10,12 +10,35 @@ import {
 class Splash extends Component {
 	render() {
 		return (
-			<Card style={{ textAlign: 'center' }}>
+			<Card
+				style={{
+					textAlign: 'center',
+					background: 'black',
+					marginTop: '4rem'
+				}}
+			>
 				<CardContent>
-					<Typography variant="headline">Welcome to Trakster!</Typography>
+					<Typography
+						variant="headline"
+						style={{ color: 'white', fontWeight: 'bold' }}
+					>
+						Welcome to Trackster
+					</Typography>
 				</CardContent>
 				<CardActions style={{ justifyContent: 'center' }}>
-					<Button color="primary" variant="raised" href="http://localhost:8888">
+					<Button
+						style={{
+							background: '#1db954',
+							'&:hover': {
+								background: '#1db500'
+							},
+							color: 'white',
+							fontWeight: 'bold',
+							padding: '1rem inherit'
+						}}
+						variant="raised"
+						href="http://localhost:8888"
+					>
 						Login with Spotify
 					</Button>
 				</CardActions>
